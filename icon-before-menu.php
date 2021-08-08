8750791d78f6bc467b2ac4068152f7bbe9bc1415
Mehdi bolourian <Mehdimblr@gmail.com>
2021-08-08 16:26:33 +0430

    Base Theme

--- icon-before-menu.php
+++ icon-before-menu.php
@@ -12,6 +12,8 @@
  * Domain Path:       /languages
  */
 
+
+// Add inputs in appreance>nav-menu
 add_action('wp_nav_menu_item_custom_fields', 'add_custom_menu_item', 10, 2);
 function add_custom_menu_item($item_id, $item)
 {
@@ -22,30 +24,46 @@ function add_custom_menu_item($item_id, $item)
     */
     $menu_item_name = 'menu_item_' . $item_id;
     $menu_item_ID = 'menu-item-' . $item_id;
+
+    //Check and get value of selcted
+    $getValue = function ($item_id, $menu_item_name) {
+        if (get_post_meta($item_id, $menu_item_name)) {
+            $menuValu = get_post_meta($item_id, $menu_item_name);
+            return "<option value=" . $menuValu[0] . " selcted> $menuValu[0] </option>";
+        } else {
+            return 0;
+        }
+    };
 ?>
     <div class="sectionCustomField">
-        <label for="menu-item-<?php echo $item_id ?>">آیکون مورد نظر را انتخاب کنید: </label>
-        <input type="hidden" class="nav-menu-id" value="<?php echo $item_id; ?>" />
-        <input type="text" class="custom-menu-item" name="<?php echo $menu_item_name ?>" id="<?php echo $menu_item_ID ?>" value="<?php if (get_post_meta($item_id,$menu_item_name)) {
-            $menuValu = get_post_meta($item_id,$menu_item_name);
-            echo $menuValu[0];
-                                                                                                                                        
-                                                                                                                                    } else {
-                                                                                                                                        echo "آیکونی را انتخاب کنید";
-                                                                                                                                    } ?>" />
+        <label for="<?php echo $menu_item_ID ?>">انتخاب آیکون</label>
+        <select id="<?php echo $menu_item_ID ?>" name="<?php echo $menu_item_name ?>">
+            <?php
+            if ($value = $getValue($item_id, $menu_item_name)) {
+                echo $value;
+            } ?>
+            <option value="dash1">s</option>
+            <option value="dash2">d</option>
+            <option value="dash3"></option>
+            <option value="dash4">s</option>
+            <option value="dashicons-id">dashicons-id</option>
+        </select>
+
     </div>
 
 <?php
 }
-
+// Save values with "wp_update_nav_menu_item" hook
 add_action('wp_update_nav_menu_item', 'save_menu_item_desc', 10, 2);
 function save_menu_item_desc($menu_id, $menu_item_db_id)
 {
     $menuItem = $_POST["menu_item_{$menu_item_db_id}"];
     if (isset($menuItem)) {
-        update_post_meta($menu_item_db_id,'menu_item_' . $menu_item_db_id, $menuItem);
+        update_post_meta($menu_item_db_id, 'menu_item_' . $menu_item_db_id, $menuItem);
     }
 }
+
+// Edit and show icon on page before menu items
 add_filter('nav_menu_item_title', 'my_nav_menu_item_title', 10, 4);
 function my_nav_menu_item_title($title, $item, $args, $depth)
 {
@@ -53,25 +71,31 @@ function my_nav_menu_item_title($title, $item, $args, $depth)
     if ($depth == 0) {
         // first level
         // global $menu_item_name;
+        $menuValu = get_post_meta($item->ID, "menu_item_" . $item->ID);
 
-        // $menuItemIconPath = get_option();
-        $menuValu = get_post_meta($item->ID,"menu_item_" . $item->ID);
-        
-        $title = $menuValu[0] . '<span class="::be">' . $title . '</span>';
+        $title = '<span class="dashicons ' . $menuValu[0] . ' ">' . '</span>' . '<span>' . $title . '</span>';
     }
 
     return $title;
 }
+
+// Load dashicons in website pages
+add_action('wp_enqueue_scripts', 'load_dashicons', 999);
 function load_dashicons()
 {
     wp_enqueue_style('dashicons');
 }
-add_action('wp_enqueue_scripts', 'load_dashicons',999);
+
+//Add custom style in admin page
 add_action('admin_enqueue_scripts', 'style_admin_icon',);
 function style_admin_icon()
 {
+    //Custom Css
     wp_enqueue_style('icon_style', plugin_dir_url(__FILE__) . 'css/style.css',);
+    
 }
+
+//Add custom style in website's page
 add_action('wp_enqueue_scripts', 'style_main_icon',);
 function style_main_icon()
 {
